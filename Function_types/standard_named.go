f5036e6c3f3fe11313aa6089c3f5b9cbbfc2c037
NazimRiyadh <neariyadh786@gmail.com>
2026-01-05 23:57:23 +0600

    function types started, standard function done

---
+++ Function_types/standard_named.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//if a func have name then it's standard function
+func add(a int, b int) int {
+	sum := a + b
+	return sum
+}
+
+func main() {
+	p, q := 10, 12
+	ans := add(p, q)
+	fmt.Println(ans)
+}
