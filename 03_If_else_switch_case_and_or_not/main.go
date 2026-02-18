058f77d1064a3c4398990ef863ca3d1983204f7d
NazimRiyadh <neariyadh786@gmail.com>
2026-02-18 19:35:35 +0600

    Update some switch case example

--- 03_If_else_switch_case_and_or_not/main.go
+++ 03_If_else_switch_case_and_or_not/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -12,8 +13,9 @@ func main() {
 		fmt.Print("Children")
 	}
 	fmt.Println()
+
 	//switch case
-	b := 2
+	b := 10
 	switch b {
 	case 1:
 		fmt.Println("You choose one")
@@ -21,7 +23,44 @@ func main() {
 		fmt.Println("You choose two")
 	case 3:
 		fmt.Println("You choose three")
+	default:
+		fmt.Println("I am here by default!")
 	}
+
+	//multiple condition switch
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's Weekend")
+	default:
+		fmt.Println("It's Workday :{")
+
+	}
+	a := 2
+	switch a {
+	case 'c', 'd':
+		fmt.Println("I am actually a letter")
+	case 1, 2:
+		fmt.Println("I am a number")
+	default:
+		fmt.Println("I donno what I am!")
+
+	}
+
+	//type Switching
+	who_am_I := func(i interface{}) {
+
+		switch i.(type) {
+		case int:
+			fmt.Println("I am a int")
+		case string:
+			fmt.Println("I am a string")
+		case bool:
+			fmt.Println("I am a bool")
+		default:
+			fmt.Println("I donno the type")
+		}
+	}
+	who_am_I("Hola!")
 }
 
 // and=&&, or= ||, not= !
